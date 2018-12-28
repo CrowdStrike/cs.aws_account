c9493a578c3b5d5fd57d4e36fed5bb94751695a0
David Davis <david_davis@crowdstrike.com>
2018-12-28 11:31:01 -0500

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,47 @@
+from setuptools import setup, find_packages
+
+version = '1.0.0'
+
+tests_require = [
+    'zope.testrunner',
+    'zope.configuration'
+]
+
+setup(name='cs.aws_account',
+      version=version,
+      description="AWS account components",
+      long_description=open("README.md").read() + "\n" +
+                       open("HISTORY.txt").read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: Other/Proprietary License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+      ],
+      author='David Davis',
+      author_email='david.davis@crowdstrike.com',
+      url='http://stash.cs.sys/projects/PSO/repos/cs.aws_account/browse',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['cs'],
+      include_package_data=True,
+      package_data = {
+          '': ['*.md', '*.zcml']
+        },
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'zope.component',
+          'zope.interface',
+          'zope.schema'
+      ],
+      extras_require={
+            'testing': tests_require
+      },
+      entry_points={
+          },
+      )
