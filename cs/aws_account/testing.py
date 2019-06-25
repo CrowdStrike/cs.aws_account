4e12f268f0a5dffefcdb0fbf64a5777efe3053dc
David Davis <davisd50@gmail.com>
2019-06-25 12:15:01 -0500

    fix testing import for Py2

--- cs/aws_account/testing.py
+++ cs/aws_account/testing.py
@@ -3,7 +3,7 @@ import warnings
 import cs.aws_account
 
 #see https://github.com/boto/boto3/issues/454
-warnings.filterwarnings("ignore", category=ResourceWarning, message="unclosed.*<ssl.SSLSocket.*>")
+warnings.filterwarnings("ignore", message="unclosed.*<ssl.SSLSocket.*>")
 
 AWS_ACCOUNT_INTEGRATION_LAYER = ZCMLFileLayer(cs.aws_account,
                                           zcml_file='ftesting.zcml',
