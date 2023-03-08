1b9b1e8749f16b7f90297728e66246854647ece1
Ross Clarke <ross.clarke@crowdstrike.com>
2023-03-08 09:20:53 -0500

    added logging for caught awsclientexception

--- cs/aws_account/regional_account.py
+++ cs/aws_account/regional_account.py
@@ -97,6 +97,7 @@ class RegionalAccount:
         try:
             return self._limited(getattr(client, method), **kwargs)
         except AWSClientException as e:
+            logger.warning(str(e))
             raise e 
 
     def get_paginator(self, service, method, client_kwargs=None):
