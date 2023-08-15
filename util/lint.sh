27320cc2b07b644eb9683ac310b5dbb0f771657b
Forrest Aldridge <forrest.aldridge@crowdstrike.com>
2023-08-14 21:48:22 -0500

    Add GitHub Actions.

--- util/lint.sh
+++ util/lint.sh
@@ -2,3 +2,4 @@
 
 flake8 "cs/aws_account" --count --exit-zero --max-complexity=15 --max-line-length=127 --statistics
 pylint "cs/aws_account" --exit-zero --max-line-length=127 --disable=R0801 --ignore=tests
+pydocstyle "cs/aws_account" --count
